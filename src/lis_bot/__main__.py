dd9b1c523917f6be13d59503bbf120d76c447c39
Aspect1103 <jack.ashwell1@gmail.com>
2023-04-13 20:48:04 +0100

    Added module __init__.py files.

--- src/lis_bot/__main__.py
+++ src/lis_bot/__main__.py
@@ -44,7 +44,7 @@ logger = logging.getLogger("discord")
 logger.setLevel(logging.DEBUG)
 handler = logging.FileHandler(filename=logPath, encoding="utf-8", mode="a")
 handler.setFormatter(
-    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s")
+    logging.Formatter("%(asctime)s:%(levelname)s:%(name)s:%(message)s"),
 )
 logger.addHandler(handler)
 
